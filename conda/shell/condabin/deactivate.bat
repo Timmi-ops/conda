ca6e36b8a823e3eda2a19eeecdc74320993c5925
Timmi-ops <172539327+Timmi-ops@users.noreply.github.com>
2024-06-13 16:38:49 +0300

    Last login: Thu Jun 13 14:25:51 on ttys002 admin@Johns-MacBook-Pro ~ % /Users/admin/Downloads/pre-commit-3.7.1/testing/resources/types_repo/bin/conda/conda/shell/bin/deactivate ; exit; DeprecationWarning: 'source deactivate' is deprecated. Use 'conda deactivate'. /Users/admin/Downloads/pre-commit-3.7.1/testing/resources/types_repo/bin/conda/conda/shell/bin/deactivate: line 4: /etc/profile.d/conda.sh: No such file or directory
    Saving session...
    ...copying shared history...
    ...saving history...truncating history files...
    ...completed.
    Deleting expired sessions...none found.
    
    [Процес завершено]

--- conda/shell/condabin/deactivate.bat
+++ conda/shell/condabin/deactivate.bat
@@ -2,3 +2,15 @@
 :: SPDX-License-Identifier: BSD-3-Clause
 @ECHO DeprecationWarning: 'deactivate' is deprecated. Use 'conda deactivate'. 1>&2
 conda.bat deactivate %*
+Last login: Thu Jun 13 14:25:51 on ttys002
+admin@Johns-MacBook-Pro ~ % /Users/admin/Downloads/pre-commit-3.7.1/testing/resources/types_repo/bin/conda/conda/shell/bin/deactivate ; exit;
+DeprecationWarning: 'source deactivate' is deprecated. Use 'conda deactivate'.
+/Users/admin/Downloads/pre-commit-3.7.1/testing/resources/types_repo/bin/conda/conda/shell/bin/deactivate: line 4: /etc/profile.d/conda.sh: No such file or directory
+
+Saving session...
+...copying shared history...
+...saving history...truncating history files...
+...completed.
+Deleting expired sessions...none found.
+
+[Процес завершено]
